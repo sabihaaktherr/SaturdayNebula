637b675b838a08a990ccec7889ac8f02b8401575
sabihaaktherr <diamondsabiha@gmail.com>
2025-08-10 17:40:48 -0400

    Local Commit

--- src/com/upskill/java_1/DataType.java
+++ src/com/upskill/java_1/DataType.java
@@ -6,7 +6,7 @@ public class DataType extends Variables {
 		System.out.println("Hello this is Sabiha");
 		
 		int a=645353455;
-		int b=36845345;
+		int b=3684534;
 	    int c= a+b;
 	    System.out.println(c); 
 	    
